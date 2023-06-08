3a729321b4f5e34ac181969155d7d05a9941ad14
Uma Mahesh <uma.mahesh@chronus.com>
2023-06-08 18:56:31 +0530

    Add user profile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
+require 'digest/md5'
 module ApplicationHelper
+    def gravatar_for(user, options = {size:100})
+        email_adr= user.email.downcase
+        hash = Digest::MD5.hexdigest(email_adr)
+        size= options[:size]
+        pic_url=  "https://www.gravatar.com/avatar/#{hash}/?s=#{size}"
+        image_tag(pic_url, alt: user.username, class: "rounded mx-auto d-block center")
+    end
 end
