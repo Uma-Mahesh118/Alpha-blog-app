253802daee3995c30b922040ecb1feb6dafd315a
Uma Mahesh <uma.mahesh@chronus.com>
2023-06-08 17:57:23 +0530

    edit, update and user profile set

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,26 @@ class UsersController < ApplicationController
     def new
         @user= User.new
     end
+
+    def show
+        @user = User.find(params[:id]) 
+    end    
+
+    def edit 
+        @user= User.find(params[:id])
+    end
+
+    def update
+        @user= User.find(params[:id])
+        if @user.update( user_params )
+            flash[:notice]= 'Update Scuccesful'
+            redirect_to user_path(@user)
+        else
+            render 'edit'
+            
+        end
+    end
+
     def create
         @user = User.new(user_params)
         if @user.save
@@ -10,7 +30,7 @@ class UsersController < ApplicationController
         else
             render 'new'
         end
-    end
+    end   
 
     private
 
